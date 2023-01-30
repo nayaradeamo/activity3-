872003ca51b888a863f64c040f95e5afe0697727
nayara <a2208030@institutmontilivi.cat>
2023-01-30 17:12:41 +0100

    Agregar archivos de proyecto.

---
+++ activity3 github/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        ConsoleKeyInfo tecla;
+        do
+        {
+            Console.Clear();
+            ShowOptions();
+            tecla = Console.ReadKey();
+            Console.Clear();
+
+            switch (tecla.Key)
+            {
+                case ConsoleKey.D1:
+                    capitals();
+                    break;
+                case ConsoleKey.D2:
+                    OddOrEven();
+                    break;
+                case ConsoleKey.D3:
+                    DoTry2ReadValidTimeStamp();
+                    break;
+                
+
+            }
+        } while (tecla.Key != ConsoleKey.D0);
+    }
+
+    public static void ShowOptions()
+    {
+        Console.WriteLine("1- Capitals");
+        Console.WriteLine("2- ");
+        Console.WriteLine("3- ");
+    }
+}
